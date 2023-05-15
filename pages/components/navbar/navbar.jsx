2deaee1d13ceff8b30830d2829efbdead568019a
AzwarKusumah <azwarmeizia.kusumah@gmail.com>
2023-05-16 00:12:16 +0800

    partial update

---
+++ pages/components/navbar/navbar.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Form,
+  Button,
+  NavDropdown,
+} from "react-bootstrap";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function NavbarFun() {
+  return (
+    <div>
+      <Navbar bg="danger" expand="lg">
+        <Container fluid className="px-5">
+          <Link href="/">
+            <Image
+              alt="logo"
+              src="/ryukonime_logo.png"
+              width={150}
+              height={70}
+            />
+          </Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse className="ms-5 " id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Nav.Link
+                className="text-light"
+                style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+                href="/daftar-komik/page/1"
+              >
+                DAFTAR KOMIK
+              </Nav.Link>
+              <Nav.Link
+                className="text-light"
+                style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+                href={`/komikk/manga/page/1`}
+              >
+                MANGA
+              </Nav.Link>
+              <Nav.Link
+                className="text-light"
+                style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+                href={`/komikk/manhwa/page/1`}
+              >
+                MANHWA
+              </Nav.Link>
+              <Nav.Link
+                className="text-light"
+                style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+                href={`/komikk/manhua/page/1`}
+              >
+                MANHUA
+              </Nav.Link>
+              <Nav.Link
+                className="text-light"
+                style={{ fontFamily: "Poppins", fontWeight: "bold" }}
+                href={`/bookmark`}
+              >
+                BOOKMARK
+              </Nav.Link>
+            </Nav>
+            <div className="d-flex" id="search">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                id="query"
+              />
+              <Button style={{ fontFamily: "Poppins" }} variant="light">
+                Search
+              </Button>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
